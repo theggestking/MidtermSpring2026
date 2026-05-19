ba1bb18fd4fd13f14dc7ddb496d4de84c1cd44da
theggestking <qarqashadze313@gmail.com>
2026-05-20 01:48:15 +0400

    Use shared legality check in game loop

--- src/Main.java
+++ src/Main.java
@@ -169,23 +169,7 @@ public class Main {
                 }
 
                 String card = hand.get(chosen);
-                boolean ok = false;
-                String cardColor = color(card);
-                String upColor = color(upCard);
-                String cardRank = rank(card);
-                String upRank = rank(upCard);
-
-                if (card.startsWith("W")) {
-                    ok = true;
-                } else if (cardColor.equals(upColor)) {
-                    ok = true;
-                } else if (!calledColor.equals("") && cardColor.equals(calledColor)) {
-                    ok = true;
-                } else if (cardRank.equals(upRank) && !cardRank.equals("NUMBER")) {
-                    ok = true;
-                } else if (cardRank.equals("NUMBER") && upRank.equals("NUMBER") && number(card) == number(upCard)) {
-                    ok = true;
-                }
+                boolean ok = isLegal(card, upCard, calledColor);
 
                 if (!ok) {
                     if (!quiet) {
