ea2dbf799e70745fc01c31eeb478c5eadfcc83b1
theggestking <qarqashadze313@gmail.com>
2026-05-20 02:08:25 +0400

    Extract action card effect handling

--- src/Main.java
+++ src/Main.java
@@ -212,37 +212,7 @@ public class Main {
                     return;
                 }
 
-                if (rank(card).equals("SKIP")) {
-                    next();
-                    next();
-                } else if (rank(card).equals("REVERSE")) {
-                    direction = direction * -1;
-                    if (playerNames.size() == 2) {
-                        next();
-                        next();
-                    } else {
-                        next();
-                    }
-                } else if (rank(card).equals("DRAW_TWO")) {
-                    next();
-                    hands.get(currentPlayer).add(draw());
-                    hands.get(currentPlayer).add(draw());
-                    if (!quiet) {
-                        System.out.println(playerNames.get(currentPlayer) + " draws two.");
-                    }
-                    next();
-                } else if (rank(card).equals("WILD_DRAW_FOUR")) {
-                    next();
-                    for (int i = 0; i < 4; i++) {
-                        hands.get(currentPlayer).add(draw());
-                    }
-                    if (!quiet) {
-                        System.out.println(playerNames.get(currentPlayer) + " draws four.");
-                    }
-                    next();
-                } else {
-                    next();
-                }
+                applyCardEffect(card);
             } else {
                 next();
             }
@@ -427,6 +397,40 @@ public class Main {
             return Integer.parseInt(card.substring(1));
         }
         return -1;
+    }
+
+    static void applyCardEffect(String card) {
+        if (rank(card).equals("SKIP")) {
+            next();
+            next();
+        } else if (rank(card).equals("REVERSE")) {
+            direction = direction * -1;
+            if (playerNames.size() == 2) {
+                next();
+                next();
+            } else {
+                next();
+            }
+        } else if (rank(card).equals("DRAW_TWO")) {
+            next();
+            hands.get(currentPlayer).add(draw());
+            hands.get(currentPlayer).add(draw());
+            if (!quiet) {
+                System.out.println(playerNames.get(currentPlayer) + " draws two.");
+            }
+            next();
+        } else if (rank(card).equals("WILD_DRAW_FOUR")) {
+            next();
+            for (int i = 0; i < 4; i++) {
+                hands.get(currentPlayer).add(draw());
+            }
+            if (!quiet) {
+                System.out.println(playerNames.get(currentPlayer) + " draws four.");
+            }
+            next();
+        } else {
+            next();
+        }
     }
 
     static int scoreForWinner(int winner) {
